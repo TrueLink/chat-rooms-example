6b33590a8fd5b42a87a63df20f3e74e5bc13ef74
Evgenus <chernyshov.eugene@gmail.com>
2014-12-15 17:21:31 +0200

    added missing files

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+﻿import react = require("react");
+import TypedReact = require("typed-react");
+import uuid = require("node-uuid");
+import client = require("browser-relay-client");
+import routing = require("browser-relay-client/lib/routing");
+import hub = require("browser-relay-client/lib/hub");
+var RD = react.DOM;
+
+export interface AppProps {
+}
+
+interface AppState {
+}
+
+class AppClass extends TypedReact.Component<AppProps, AppState> {
+    getInitialState(): AppState {
+        return {
+        };
+    }
+
+    componentDidMount() {
+        var hub = this.props.hub;
+    }
+
+    componentWillUnmount() {
+        var hub = this.props.hub;
+    }
+
+    render() {
+        var hub = this.props.hub;
+        return RD.div(null,
+            RD.h1(null, "GUID: ", RD.span({
+                className: "guid"
+            }, hub.guid))
+        );
+    }
+}
+
+export var App = react.createFactory(TypedReact.createClass<AppProps, AppState>(AppClass));
+
+window.addEventListener("load", function () {
+    var guid = uuid.v4();
+    var instance = hub.Hub.create(guid);
+    var app = App({
+        hub: instance,
+        ramps: [
+            "ws://127.0.0.1:20500/",
+            "ws://127.0.0.1:20501/"
+        ]
+    });
+    react.render(app, document.body);
+});
+
+window.addEventListener("error", function () {
+    console.log.call(console, arguments);
+});
---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../node_modules/browser-relay-client/index.d.ts" />
+/// <reference path="../node_modules/typed-react/dist/typed-react.d.ts" />
+/// <reference path="../node_modules/typed-react/typings/react/react.d.ts" />
+/// <reference path="node-uuid/node-uuid.d.ts" />
+/// <reference path="node/node.d.ts" />
