127e699346f73e90d9f74b50e66fc91da469bbdc
Evgenus <chernyshov.eugene@gmail.com>
2014-12-17 16:57:14 +0200

    added routing display

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import uuid = require("node-uuid");
 import client = require("browser-relay-client");
 import routing = require("browser-relay-client/lib/routing");
 import hub = require("browser-relay-client/lib/hub");
+import rampConnection = require("./ramp-connection");
 var RD = react.DOM;
 
 export interface AppProps {
@@ -13,21 +14,62 @@ export interface AppProps {
 
 interface AppState {
     currentRamps?: string[];
+    contacts?: string[];
+    routing?: string[][];
 }
 
 class AppClass extends TypedReact.Component<AppProps, AppState> {
     getInitialState(): AppState {
         return {
             currentRamps: this.props.ramps,
+            routing: []
         };
+    }
+
+    private _addRamp(event: React.FormEvent) {
+        var hub = this.props.hub;
+        var form = <HTMLFormElement>event.target;
+        var addr = <HTMLInputElement>form.elements.item("addr");
+        if (!addr.value) return false;
+
+        var ramps = this.state.currentRamps;
+        ramps.push(addr.value);
+
+        this.setState({
+            currentRamps: ramps
+        });
+
+        form.reset();
+
+        event.preventDefault();
+        return false;
+    }
+
+    private _routingChanged(table: routing.RoutingTable): void {
+        var nodes = table.children;
+        var hub = this.props.hub;
+        var contacts: string[] = [];
+
+        for (var i = 0; i < nodes.length; i++) {
+            var guid = nodes[i];
+            if (guid == hub.guid) continue;
+            contacts.push(guid);
+        }
+
+        this.setState({
+            contacts: contacts,
+            routing: table.serialize()
+        });
     }
 
     componentDidMount() {
         var hub = this.props.hub;
+        hub.onRoutingChanged.on(this._routingChanged, this);
     }
 
     componentWillUnmount() {
         var hub = this.props.hub;
+        hub.onRoutingChanged.off(this._routingChanged, this);
     }
 
     render() {
@@ -35,7 +77,38 @@ class AppClass extends TypedReact.Component<AppProps, AppState> {
         return RD.div(null,
             RD.h1(null, "GUID: ", RD.span({
                 className: "guid"
-            }, hub.guid))
+            }, hub.guid)),
+            RD.h2(null, "Ramp servers"),
+            this.state.currentRamps.map((addr) => {
+                return rampConnection.RampConnection({
+                    key: addr,
+                    hub: hub,
+                    addr: addr
+                });
+            }),
+            RD.form({
+                onSubmit: this._addRamp
+            },
+                RD.input({ type: "text", name: "addr" }),
+                RD.input({ type: "submit", value: "Add" })
+            ),
+            RD.h2(null, "Routing table"),
+            RD.table({ className: "routing" },
+                RD.thead(null,
+                    RD.tr(null,
+                        RD.th(null, "Origin"),
+                        RD.th(null, "Endpoint")
+                        )
+                    ),
+                RD.tbody(null,
+                    this.state.routing.map((row) => {
+                        return RD.tr(null,
+                            RD.td({ className: "guid" }, row[0]),
+                            RD.td(null, row[2])
+                            );
+                    })
+                    )
+                )
         );
     }
 }
---
+++ src/ramp-connection.ts
@@ -0,0 +1,133 @@
+﻿import react = require("react");
+import TypedReact = require("typed-react");
+import uuid = require("node-uuid");
+import hub = require("browser-relay-client/lib/hub");
+import connection = require("browser-relay-client/lib/connection");
+import wsConn = require("browser-relay-client/lib/websocket-connection");
+
+var RD = react.DOM;
+
+export interface RampConnectionProps {
+    addr: string;
+    hub: hub.HubAPI;
+}
+
+interface RampConnectionState {
+    id?: string;
+    connection?: wsConn.WebSocketConnectionAPI;
+    peers?: string[];
+    uid?: string;
+}
+
+class RampConnectionClass extends TypedReact.Component<RampConnectionProps, RampConnectionState> {
+    getInitialState(): RampConnectionState {
+        return {
+            peers: [],
+            uid: uuid.v4()
+        };
+    }
+
+    private _connectionIdentified(data: connection.IdentificationData) {
+        this.setState({
+            id: data.endpoint
+        });
+    }
+
+    private _peerConnected(id: string) {
+        var peers = this.state.peers;
+        var index = peers.indexOf(id);
+        if (index >= 0) return;
+        peers.push(id);
+        this.setState({
+            peers: peers.sort()
+        });
+    }
+
+    private _peerDisconnected(id: string) {
+        var peers = this.state.peers;
+        var index = peers.indexOf(id);
+        if (index == -1) return;
+        peers.splice(index, 1);
+        this.setState({
+            peers: peers
+        });
+    }
+
+    private _connect() {
+        var hub = this.props.hub;
+        var connection = hub.connect(this.props.addr);
+        connection.onIdentified.on(this._connectionIdentified, this);
+        connection.onConnected.on(this._peerConnected, this);
+        connection.onDisconnected.on(this._peerDisconnected, this);
+        connection.onClose.on(this._disconnected, this);
+
+        this.setState({
+            connection: connection,
+        });
+    }
+
+    private _disconnected() {
+        var connection = this.state.connection;
+        connection.onIdentified.off(null, this);
+        connection.onConnected.off(null, this);
+        connection.onDisconnected.off(null, this);
+        this.replaceState(this.getInitialState());
+    }
+
+    private _disconnect() {
+        var hub = this.props.hub;
+        hub.disconnect(this.props.addr);
+    }
+
+    componentWillUnmount() {
+        var hub = this.props.hub;
+        var connection = this.state.connection;
+        if (connection) {
+            connection.onIdentified.off(null, this);
+            connection.onConnected.off(null, this);
+            connection.onDisconnected.off(null, this);
+        }
+    }
+
+    render() {
+        var addrId = "ramp-addr-" + this.state.uid;
+        var connId = "conn-id-" + this.state.uid;
+        return RD.div({
+            className: "connection"
+        },
+            this.state.connection
+            ? RD.button({ onClick: this._disconnect }, "Disconnect")
+            : RD.button({ onClick: this._connect }, "Connect"),
+            RD.table(null, 
+                RD.tbody(null,
+                    RD.tr(null, 
+                        RD.td(null, RD.label({
+                            htmlFor: addrId
+                        }, "Address")),
+                        RD.td(null, RD.input({
+                            id: addrId,
+                            readOnly: true,
+                            value: this.props.addr
+                        }))
+                        ),
+                    RD.tr(null,
+                        RD.td(null, RD.label({
+                            htmlFor: connId
+                        }, "ID")),
+                        RD.td(null, RD.input({
+                            id: connId,
+                            readOnly: true,
+                            value: this.state.id
+                        }))
+                        ),
+                    RD.tr(null,
+                        RD.td(null, "Peers"),
+                        RD.td(null, this.state.peers.join(", "))
+                        )
+                )
+            )
+        );
+    }
+}
+
+export var RampConnection = react.createFactory(TypedReact.createClass<RampConnectionProps, RampConnectionState>(RampConnectionClass));
